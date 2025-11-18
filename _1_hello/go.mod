ccb472786b008a579d5aa66ea01c6e92ae7adb53
abanoyb_boshra <abanoub.boshra.anis.com>
2025-11-18 22:20:45 +0200

    tests

--- _1_hello/go.mod
+++ _1_hello/go.mod
@@ -1,3 +1,5 @@
 module hello
 
 go 1.22.5
+
+require github.com/google/go-cmp v0.7.0
--- _1_hello/main.go
+++ _1_hello/main.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 
 	"hello/morestrings"
+
+	//Import Remote Packagers
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello, Go!"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
