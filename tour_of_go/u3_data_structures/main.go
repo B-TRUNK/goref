925ad157ddf7678592a3234065d915dd069aa461
abanoyb_boshra <abanoub.boshra.anis.com>
2025-11-29 19:03:36 +0200

    maps

--- tour_of_go/u3_data_structures/main.go
+++ tour_of_go/u3_data_structures/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"ds/arrays"
+	"ds/maps"
 	"ds/pointers"
 	"ds/strct"
 	"fmt"
@@ -11,10 +12,12 @@ func main() {
 
 	fmt.Println("This Unit is All About Data-Structures")
 
+	fmt.Println("Pointers :")
 	pointers.PNT()
 
 	strct.STRUCTURES()
 
+	fmt.Println("Arrays :")
 	arrays.Array()
 	arrays.Slices()
 	arrays.Slices_as_refs()
@@ -26,8 +29,12 @@ func main() {
 	arrays.Slices_of_slices()
 	arrays.Slices_appending()
 
+	fmt.Println("Ranges: ")
 	arrays.Range()
 	arrays.Range2()
 	//arrays.Pic()
+
+	fmt.Println("Maps :")
+	maps.Maps()
 
 }
