36e902887c0aa1c699e077f5f0e42f13f8b518ab
abanoyb_boshra <abanoub.boshra.anis.com>
2025-12-02 18:51:46 +0200

    add_answers_final

---
+++ tour_of_go/u1_basics/testing/reverse_test.go
@@ -0,0 +1,27 @@
+package testing
+
+import "testing"
+
+func TestReverseRunes(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := ReverseRunes(c.in)
+		if got != c.want {
+			t.Errorf("ReverseRunes(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+/*
+
+go test
+PASS
+ok      bsq/testing     0.002s
+
+*/
